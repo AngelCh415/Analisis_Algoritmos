8b66ca304f06f9678033747dffcf2a35e5203a35
AngelCh415 <angelchavezmorones@gmail.com>
2021-11-03 21:38:15 -0600

    Codificador al 90

--- encode.c
+++ encode.c
@@ -5,7 +5,6 @@
 char cadena[1000];
 unsigned char cara[100];
 unsigned int cubeta[100];
-unsigned char Huffman[1000][2];//[][1] -> Letra, [][2] -> Frecuencia
 typedef struct hoja{
     char letra;
     int frec;
@@ -19,6 +18,7 @@ typedef struct arbol{
     int cap;
     nodo ** hojas;
 }arbol;
+
 struct hoja* crearNodo(unsigned char letra, unsigned frec)
 {
 	struct hoja* temp = (struct hoja*)malloc(sizeof(struct hoja));
@@ -27,6 +27,7 @@ struct hoja* crearNodo(unsigned char letra, unsigned frec)
 	temp->frec = frec;
 	return temp;
 }
+unsigned char codigos[256][2];//[letra][0]->codigo de la letra, [letra][1]->codigo de la longitud (servira para binario)
 
 // A utility function to create
 // a min heap of given cap
@@ -115,7 +116,7 @@ void buildarbol(struct arbol* arbol)
 }
 
 // A utility function to print an hojas of tama n
-void printArr(int arr[], int n)
+void printArr(char arr[], int n)
 {
 	int i;
 	for (i = 0; i < n; ++i)
@@ -149,36 +150,102 @@ struct hoja* HuffmanTree(struct hoja ** hojas, int tama)
 	return pop(arbol);
 }
 
-void printCodes(struct hoja* root, int arr[],int top)
+void printCodes(struct hoja* root, int lon,int top)
 {
 	if (root->der) {
-		arr[top] = 0;
-		printCodes(root->der, arr, top + 1);
+		printCodes(root->der, lon *2, top + 1);
+	}
+	if (root->izq) {
+		printCodes(root->izq, lon *2+1, top + 1);
+	}
+	if (root->izq == NULL && root->der == NULL) {
+		printf("%c:%d ", root->letra,lon);
+		codigos[root->letra][0] = lon;
+		codigos[root->letra][1] = top;
+	}
+}
+//Reversa
+char* reversestring(char *cadena,int tama)
+{
+	int i,j;
+	char temp;
+	for(i=0,j=tama-1;i<j;i++,j--)
+	{
+		temp=cadena[i];
+		cadena[i]=cadena[j];
+		cadena[j]=temp;
+	}
+	return cadena;
+}
+//Pasa de decimal a binario
+char * decToBin(int num, int n)
+{
+	char *bin = (char *)malloc(sizeof(char) * n);
+	int i = 0;
+	while (num != 0) {
+		bin[i++] = (num & 1) + '0';
+		num = num >> 1;
+	}
+	while (i < n)
+		bin[i++] = '0';
+	return reversestring(bin, n);
+}
+void codificar(struct hoja* root,int tama)
+{
+	int aux = 10000000,recorrido = 8;//1<<7
+	unsigned char ans = 0;
+	char *arreB;
+	printf("Codificando\n");
+	for(int i = 0; i < tama; i++){
+		arreB= decToBin(codigos[cara[i]][0], codigos[cara[i]][1]);
+		int tamrepe = codigos[cara[i]][1];//r - 1 , o - 2
+		printf("\n");
+		printf("%c: Valor decimal %d, Codigo Binario ", cara[i],codigos[cara[i]][0]);
+		for(int j = 0; j < tamrepe; j++){
+			printf("%c",arreB[j]);
+			if(arreB[j] == '1'){
+				ans = ans | recorrido;
+			}
+			recorrido = recorrido >> 1;
+			if(recorrido == 0){
+				//se meteria a un archivo
+				
+				recorrido = 8;
+				ans=0;
+			}
+		}
 	}
 	
-	if (root->izq) {
-		arr[top] = 1;
-		printCodes(root->izq, arr, top + 1);
-	}
-
-	if (root->izq == NULL && root->der == NULL) {
-		printf("%c: ", root->letra);
-		printArr(arr, top);
-	}
 }
-
 void HuffmanCodes(nodo ** hojas, int tama)
-
 {
 	struct hoja* root= HuffmanTree(hojas, tama);
-	int arr[100], top = 0;
+	int arr=0, top = 0;
 	printCodes(root, arr, top);
+
+	/*
+	abc
+	a: 1
+	b: 10
+	c: 11
+	Posicion 0 es la a con longitud 1 con codificacion 1
+	Posicion 1 es la b con longitud 2 con codificacion 10
+	Posicion 2 es la c con longitud 2 con codificacion 11
+	Recorrer la codificacion 
+	Si encuentro un 1
+		resultado va a hacer el corrimientio a la izquierda las veces que le toque, ejemplo
+		si en la posicion 1 hay un 1 se recorre 7 veces a la izquierda, si en la posicion 2 hay un 1
+		se recorre 6 veces a la izquierda, etc
+	Cuando supere los8 bits es nueva linea
+	*/
+	codificar(root,tama);
 }
 int cub [256];
-unsigned int cad [1000][2];
+unsigned char cad [1000][2];//[0] = caracter, [1] = frecuencia
 int main()
 {
     int i = 0,n=0;
+
     scanf("%[^\n]",cadena);
     for(i=0;i<strlen(cadena);i++)
     {
@@ -197,14 +264,12 @@ int main()
 	}
 	nodos = (nodo**)malloc(sizeof(nodo*)*n);
 	i = 0;
-
 	while (n>0)
 	{ 
 		nodos[i] = crearNodo(cara[i], cubeta[i]);
 		n--;
 		i++;
 	}
-	
     HuffmanCodes(nodos,i);
     return 0;
 }
